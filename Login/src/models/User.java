b0bdac66219368ef15f97598edb575a53e7f9961
-c <-c>
2026-02-16 12:34:24 +0100

    feat: User creation and management
    - UserRole Enum made with Standard Client and Admin Roles
    
    - User set/get made and ShoppingCart set/get.

--- Login/src/models/User.java
+++ Login/src/models/User.java
@@ -1,4 +1,27 @@
 package models;
 
 public class User {
+    private String name;
+    private ShoppingCart shoppingCart;
+
+    public User() {
+        this.name = "";
+        this.shoppingCart = new ShoppingCart();
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setShoppingCart(ShoppingCart shoppingCart) {
+        this.shoppingCart = shoppingCart;
+    }
+
+    public ShoppingCart getShoppingCart() {
+        return this.shoppingCart;
+    }
 }
--- Login/src/models/UserRole.java
+++ Login/src/models/UserRole.java
@@ -1,4 +1,20 @@
 package models;
 
+/**
+ * Définit les rôles possibles pour un utilisateur du système.
+ */
+
 public enum UserRole {
+    ADMIN("Administrateur"),
+    CLIENT("Client standard");
+
+    private final String label;
+
+    UserRole(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
 }
