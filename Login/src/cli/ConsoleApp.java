64ee436aa477e9c4fa9b6e53724518623953aa67
-c <-c>
2026-02-18 09:35:07 +0100

    feat: MenuHandler class and ConsoleApp management
    - Setup how the application in the cli will work.
    - Implements System.out.println for Navigation.

--- Login/src/cli/ConsoleApp.java
+++ Login/src/cli/ConsoleApp.java
@@ -1,4 +1,25 @@
 package cli;
 
+import repository.InMemoryProductRepo;
+import repository.ProductRepository;
+import repository.UserRepository;
+
+import java.util.Scanner;
+
 public class ConsoleApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ProductRepository productRepo = new InMemoryProductRepo();
+        UserRepository userRepo = new InMemoryUserRepo();
+
+        SeedData.init(productRepo, userRepo);
+
+        MenuHandler menu = new MenuHandler(scanner, productRepo, userRepo);
+
+        System.out.println("=== BIENVENUE SUR VOTRE SHOP V2.0 ===");
+        menu.start(); // Lance la boucle while(true)
+
+        scanner.close();
+    }
 }
