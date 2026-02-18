a2696891e9befe3f631d50dea98f9cd09d999bc6
-c <-c>
2026-02-18 13:26:31 +0100

    feat : UserBuilder management added.
    - Name (is needed)
    - Email (is needed)
    - Role (by default)

--- Login/src/factory/UserBuilder.java
+++ Login/src/factory/UserBuilder.java
@@ -1,5 +1,31 @@
 package factory;
 
-public class UserBuilder {
+import models.User;
 
+public class UserBuilder {
+    private String name;
+    private String email;
+    private String role = "CLIENT"; // par défaut
+
+    public UserBuilder withName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public UserBuilder withEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public UserBuilder withRole(String role) {
+        this.role = role;
+        return this;
+    }
+
+    public User build() {
+        if (name == null || email == null) {
+            throw new IllegalStateException("Pseudo et Email sont obligatoires.");
+        }
+        return new User(name, email, role);
+    }
 }
